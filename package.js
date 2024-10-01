4fe65d50d77ab9eef519f1b554e32fe79adb8e15
drone1 <jonlippincott@gmail.com>
2024-10-02 00:22:43 +0200

    Bump package version from 0.1.0 => 0.2.0

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'tunifight:loggedin-mixin',
-  version: '0.1.0',
+  version: '0.2.0',
   summary: 'A simple logged-in and roles check mixin to use with mdg:validated-method package',
   git: 'https://github.com/nabiltntn/loggedin-mixin.git',
   documentation: 'README.md'
